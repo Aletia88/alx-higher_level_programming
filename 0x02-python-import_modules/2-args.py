c9a88c314646971973ec65343040e1ed332687fa
Aletia88 <Abababmgmgmg@gmail.com>
2022-08-25 04:35:03 -0700

    importing module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-arg = len(sys.argv) - 1
-if (len(sys.argv) == 1):
-    print("0 arguments.")
-elif(len(sys.argv) == 2):
-    print("1 argument: ")
-else:
-    print ("{} arguments:".format(arg))
-for length in range(1, len( sys.argv)):
-    print("{}: {}".format(length, sys.argv[length]))
+if __name__ == "__main__":
+    import sys
+    arg = len(sys.argv) - 1
+    if (len(sys.argv) == 1):
+        print("0 arguments.")
+    elif(len(sys.argv) == 2):
+        print("1 argument: ")
+    else:
+        print ("{} arguments:".format(arg))
+    for length in range(1, len( sys.argv)):
+        print("{}: {}".format(length, sys.argv[length]))
