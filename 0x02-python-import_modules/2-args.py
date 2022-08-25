20bc1b088dfaa839252372e59b088253efdeeaa5
Aletia88 <Abababmgmgmg@gmail.com>
2022-08-25 04:31:24 -0700

    importing module

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-import sys 
+import sys
+arg = len(sys.argv) - 1
+if (len(sys.argv) == 1):
+    print("0 arguments.")
+elif(len(sys.argv) == 2):
+    print("1 argument: ")
+else:
+    print ("{} arguments:".format(arg))
 for length in range(1, len( sys.argv)):
     print("{}: {}".format(length, sys.argv[length]))
