e71b1350b3b5c3ad9a7588023c495a62d6b3cf9a
Aletia88 <Abababmgmgmg@gmail.com>
2022-08-31 21:25:09 -0700

    unique additon

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     _sum = 0
-    set_list = set(my_list.copy())
+    set_list = set(my_list)
     for i in set_list:
-        _sum += 1
+        _sum += i
     return _sum
