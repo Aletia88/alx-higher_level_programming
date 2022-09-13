8c10cbea9ecce55a172a38dd2f41f28618d248ea
Aletia88 <Abababmgmgmg@gmail.com>
2022-09-13 06:21:49 -0700

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Defines a class Square"""
 
 class Square:
+    """define the length of square"""
     def __init__(self, size):
+        """Initialize the data"""
         self.__size = size
