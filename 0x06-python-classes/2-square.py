653aecaf6a63b7ce098cc9fe623e8080745ba689
Samrawit Girma <abababmgmgmg@gmail.com>
2022-10-06 16:17:32 -0700

    more on classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,5 +1,7 @@
-#!/usr/bin/pyhton3
+#!/usr/bin/python3
+
 """Define a class Square"""
+
 
 class Square:
     """Size validation"""
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""define the Square class"""
+
+"""Define a class Square"""
+
 
 class Square:
-    """ """
+    """Area of a square"""
+
     def __init__(self, size=0):
-        """squre is initaialized"""
-        if type(size) is not int:
+        """Initialize a new square"""
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if type(size) is int:
-            if size >= 0:
-                self.__size = size
-            else:
-                raise ValueError("size must be >=0")
-    def area(slef):
-        self.area = size * size
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
